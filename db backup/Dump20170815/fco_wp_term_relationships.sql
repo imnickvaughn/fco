490f7e042a315540372dae4b36de9a00cac87fb8
vaughnick <vaughnick@gmail.com>
2017-08-15 16:09:32 -0400

    forgot to backup the db again
    db versioning

---
+++ db backup/Dump20170815/fco_wp_term_relationships.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: fco
+-- ------------------------------------------------------
+-- Server version	5.7.14
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `wp_term_relationships`
+--
+
+DROP TABLE IF EXISTS `wp_term_relationships`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `wp_term_relationships` (
+  `object_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `term_taxonomy_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `term_order` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`object_id`,`term_taxonomy_id`),
+  KEY `term_taxonomy_id` (`term_taxonomy_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `wp_term_relationships`
+--
+
+LOCK TABLES `wp_term_relationships` WRITE;
+/*!40000 ALTER TABLE `wp_term_relationships` DISABLE KEYS */;
+INSERT INTO `wp_term_relationships` VALUES (135,4,0),(73,3,0),(72,3,0),(71,3,0),(96,2,0),(137,4,0),(139,4,0),(141,4,0),(143,4,0),(147,4,0),(149,4,0),(151,5,0),(151,4,0),(149,5,0),(147,5,0),(145,5,0),(145,4,0),(143,6,0),(141,6,0),(139,5,0),(137,6,0),(135,6,0),(133,5,0),(133,4,0),(131,5,0),(131,4,0),(129,6,0),(129,4,0),(189,2,0),(190,2,0),(208,10,0),(230,2,0),(218,9,0),(213,8,0),(425,11,0),(409,10,0),(412,10,0),(415,10,0),(435,10,0),(461,9,0),(432,10,0),(429,11,0),(427,11,0),(428,11,0),(447,9,0),(450,9,0),(488,1,0),(495,9,0),(3,1,0),(519,1,0),(633,1,0),(3,9,0),(872,1,0),(578,5,0),(498,1,0),(578,8,0),(895,10,0),(895,9,0);
+/*!40000 ALTER TABLE `wp_term_relationships` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-08-15 16:09:08
---
+++ db backup/Dump20170815/fco_wp_yoast_seo_links.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: fco
+-- ------------------------------------------------------
+-- Server version	5.7.14
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `wp_yoast_seo_links`
+--
+
+DROP TABLE IF EXISTS `wp_yoast_seo_links`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `wp_yoast_seo_links` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `post_id` bigint(20) unsigned NOT NULL,
+  `target_post_id` bigint(20) unsigned NOT NULL,
+  `type` varchar(8) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `link_direction` (`post_id`,`type`)
+) ENGINE=MyISAM AUTO_INCREMENT=169 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `wp_yoast_seo_links`
+--
+
+LOCK TABLES `wp_yoast_seo_links` WRITE;
+/*!40000 ALTER TABLE `wp_yoast_seo_links` DISABLE KEYS */;
+INSERT INTO `wp_yoast_seo_links` VALUES (1,'#',87,0,'internal'),(2,'#',87,0,'internal'),(3,'#',87,0,'internal'),(4,'http://www.wrapgenius.me/',87,0,'external'),(5,'http://www.wrapgenius.me/',87,0,'external'),(6,'http://www.debbiemillman.com/designmatters/',87,0,'external'),(7,'http://www.debbiemillman.com/designmatters/',87,0,'external'),(8,'http://run4tiger.com/',87,0,'external'),(9,'http://run4tiger.com/',87,0,'external'),(10,'http://www.sbs.com.au/theboat/',87,0,'external'),(11,'http://www.sbs.com.au/theboat/',87,0,'external'),(12,'http://knowlupus.org/',87,0,'external'),(13,'http://knowlupus.org/',87,0,'external'),(14,'http://khan.github.io/tota11y/',87,0,'external'),(15,'http://khan.github.io/tota11y/',87,0,'external'),(16,'http://www.masitupungato.com/',87,0,'external'),(17,'http://www.masitupungato.com/',87,0,'external'),(18,'https://www.ibm.com/design/',87,0,'external'),(19,'https://www.ibm.com/design/',87,0,'external'),(20,'http://www.legworkstudio.com/',87,0,'external'),(21,'http://www.legworkstudio.com/',87,0,'external'),(22,'http://thestlbrowns.com/',87,0,'external'),(23,'http://thestlbrowns.com/',87,0,'external'),(24,'http://www.filippobello.com',87,0,'external'),(25,'http://www.filippobello.com',87,0,'external'),(26,'http://www.histography.io/',87,0,'external'),(27,'http://www.histography.io/',87,0,'external'),(28,'mailto:i&#110;&#102;&#111;&#64;&#65;&#108;lS&#79;F&#84;b&#101;ar.c&#111;&#109;',89,0,'external'),(29,'http://www.ectobox.com/clients/',171,0,'external'),(80,'https://well.blogs.nytimes.com/2013/03/25/looking-for-evidence-that-therapy-works/',213,0,'external'),(78,'http://narrowdesign.com/',213,0,'external'),(79,'http://narrowdesign.com/',213,0,'external'),(165,'#',225,0,'internal'),(107,'https://hackaday.io/project/19033-1k-lcd-tinyfont',218,0,'external'),(106,'https://github.com/LastZactionHero/0spec_avr',218,0,'external'),(105,'http://caffeine.develop.vodka/',218,0,'external'),(103,'https://github.com/LastZactionHero/boulder_tubing',218,0,'external'),(104,'https://hackaday.io/project/13022-zookeeper',218,0,'external'),(102,'https://www.cnet.com/news/trip-through-your-mp3s-with-playlost/',218,0,'external'),(101,'https://gist.github.com/LastZactionHero/cd3ac6998a94a06c37ad',218,0,'external'),(59,'http://www.localhost/allsoftbear/branding-vs-marketing/',99,208,'internal'),(58,'http://www.localhost/allsoftbear/real-role-design-portfolio-website/',99,213,'internal'),(57,'http://www.localhost/allsoftbear/ten-years-worthless-side-projects/',99,218,'internal'),(56,'#',99,0,'internal'),(54,'#',99,0,'internal'),(55,'#',99,0,'internal'),(100,'http://www.plantwithbloom.com/',218,0,'external'),(99,'https://www.solidsmack.com/resources/rent-build-wants-rent-3d-printer-delivered-door/',218,0,'external'),(168,'http://www.localhost/allsoftbear/ten-years-worthless-side-projects/',225,218,'internal'),(167,'http://www.localhost/allsoftbear/branding-vs-marketing/',225,208,'internal'),(166,'http://www.localhost/allsoftbear/real-role-design-portfolio-website/',225,213,'internal'),(164,'#',225,0,'internal'),(163,'#',225,0,'internal');
+/*!40000 ALTER TABLE `wp_yoast_seo_links` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-08-15 16:09:07
